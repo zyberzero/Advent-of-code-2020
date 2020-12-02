5f5f671d6274c0a075032fbc7f9567899c1a332e
Fritiof Hedman <989352+zyberzero@users.noreply.github.com>
2020-12-02 17:27:39 +0100

    Run code through formatter

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,34 +1,56 @@
 ﻿open System.IO
 
-type PasswordRule = { Letter:char; minOccurs:int; maxOccurs:int }
-type PasswordRow = { Rule:PasswordRule; Password:string; }
+type PasswordRule =
+    { Letter: char
+      minOccurs: int
+      maxOccurs: int }
 
-let parseRow (row:string):PasswordRow =
-                   let parseRule (rule:string)  =
-                       let parts = rule.Split ' '
-                       let char = parts.[1].[0]
-                       let occurs = (parts.[0]).Split '-'
-                       let min = occurs.[0] |> int
-                       let max = occurs.[1] |> int
-                       {Letter=char; minOccurs=min; maxOccurs=max}
-                   let parts = row.Split ':'
-                   let rule = parseRule parts.[0]
-                   let password = (parts.[1])
-                   {Rule = rule; Password = password}
+type PasswordRow =
+    { Rule: PasswordRule
+      Password: string }
 
-let verifyRowPart1 {Rule=rule;Password=password} =
-     let {minOccurs = min; maxOccurs = max; Letter=char;} = rule;
-     let chars = password.ToCharArray() |> List.ofSeq |> List.filter (fun c -> c = char) |> List.length
-     chars >= min && chars <= max
+let parseRow (row: string): PasswordRow =
+    let parseRule (rule: string) =
+        let parts = rule.Split ' '
+        let char = parts.[1].[0]
+        let occurs = (parts.[0]).Split '-'
+        let min = occurs.[0] |> int
+        let max = occurs.[1] |> int
+        { Letter = char
+          minOccurs = min
+          maxOccurs = max }
 
-let verifyRowPart2 {Rule=rule;Password=password} =
-     let {minOccurs = min; maxOccurs = max; Letter=char;} = rule;
-     (password.[min] = char) <> (password.[max] = char)
-     
+    let parts = row.Split ':'
+    let rule = parseRule parts.[0]
+    let password = (parts.[1])
+    { Rule = rule; Password = password }
+
+let verifyRowPart1 { Rule = rule; Password = password } =
+    let { minOccurs = min; maxOccurs = max; Letter = char } = rule
+
+    let chars =
+        password.ToCharArray()
+        |> List.ofSeq
+        |> List.filter (fun c -> c = char)
+        |> List.length
+
+    chars >= min && chars <= max
+
+let verifyRowPart2 { Rule = rule; Password = password } =
+    let { minOccurs = min; maxOccurs = max; Letter = char } = rule
+    (password.[min] = char) <> (password.[max] = char)
+
 [<EntryPoint>]
 let main argv =
     let input = File.ReadLines "input.txt"
-    let calculateValidPasswords  verifier =  (input |> List.ofSeq |> List.map (fun l -> l |> parseRow |> verifier) |> List.filter id |> List.length)
+
+    let calculateValidPasswords verifier =
+        (input
+         |> List.ofSeq
+         |> List.map (fun l -> l |> parseRow |> verifier)
+         |> List.filter id
+         |> List.length)
+
     printfn "Part 1: %i" (calculateValidPasswords verifyRowPart1)
     printfn "Part 2: %i" (calculateValidPasswords verifyRowPart2)
     0 // return an integer exit code
